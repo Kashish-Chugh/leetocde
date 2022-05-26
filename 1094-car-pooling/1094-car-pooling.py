13ab90b9817fa5c03488cee8804735507821af56
Kashish Chugh <chughkashish12@gmail.com>
2022-05-26 20:00:39 +0530

    Time: 7564 ms (5.01%), Space: 14.4 MB (88.16%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,16 +1,20 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        maxi=float('-inf')
-        for i,j,k in trips:
-            maxi=max(maxi,k)
-        trips=sorted(trips, key=lambda x: x[1])
-        ct=0
-        for i in range(maxi+1):
-            for j,k,l in trips:
-                if i==k:
-                    ct+=j
-                if i==l:
-                    ct-=j
-                if ct>capacity:
-                    return False
+        tot = [-1 for i in range(capacity)]
+        trips = sorted(trips, key=lambda x: x[1])
+        for j in trips:
+            p = j[0]
+            s = j[1]
+            e = j[2]
+            temp = 0
+            for i in range(len(tot)):
+                if tot[i] <= s:
+                    tot[i] = -1
+                if tot[i] == -1 and temp < p:
+                    tot[i] = e
+                    temp += 1
+                if temp == p:
+                    break
+            if temp != p:
+                return False
         return True
