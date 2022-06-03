80f617601ebc28d0a4322b30f3d26898aaa7bf5f
Kashish Chugh <chughkashish12@gmail.com>
2022-06-04 02:00:42 +0530

    Time: 248 ms (44.41%), Space: 15.1 MB (23.93%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,15 +1,11 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        dp = [None] * (len(nums))
-        dp[len(nums)-1] = 0
-        for i in range(len(nums)-2, -1, -1):
-            if nums[i] > 0:
-                minn = 999999
-                for j in range(1, nums[i]+1):
-                    if i+j >= len(nums):
-                        break
-                    if dp[i+j] != None:
-                        minn = min(minn, dp[i+j])
-                if minn != 999999:
-                    dp[i] = minn + 1
-        return dp[0]
+        if len(nums) == 1:
+            return 0
+        l,r = 0, nums[0]
+        times = 1
+        while r < len(nums)-1:
+            times+=1
+            nxt = max(i + nums[i] for i in range(l,r+1))
+            l,r = r, nxt
+        return times
